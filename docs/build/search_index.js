b06e472c518ee22ccccc8a443d9528768e91b24f
Ben Conrad <ben@benconrad.net>
2022-07-27 11:47:52 -0500

    remove build

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#BeltTransmission.jl-Documentation","page":"BeltTransmission.jl Documentation","title":"BeltTransmission.jl Documentation","text":"","category":"section"},{"location":"","page":"BeltTransmission.jl Documentation","title":"BeltTransmission.jl Documentation","text":"CurrentModule= BeltTransmission","category":"page"},{"location":"","page":"BeltTransmission.jl Documentation","title":"BeltTransmission.jl Documentation","text":"Modules=[BeltTransmission]","category":"page"}]
-}
